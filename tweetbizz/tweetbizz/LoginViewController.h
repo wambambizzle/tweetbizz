91faf8b545c3e833f61d34870eb45a304fc8bac7
Jordan Anderson <mr.janderson14@gmail.com>
2015-06-11 19:00:57 -0400

    Set up basic storyboard layout for Login view

--- tweetbizz/tweetbizz/LoginViewController.h
+++ tweetbizz/tweetbizz/LoginViewController.h
@@ -1,16 +1,14 @@
 //
-//  ViewController.h
+//  LoginViewController.h
 //  tweetbizz
 //
-//  Created by Jordan Anderson on 6/10/15.
+//  Created by Jordan Anderson on 6/11/15.
 //  Copyright (c) 2015 Jordan Anderson. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import <Parse/Parse.h>
 
-@interface ViewController : UIViewController
-
+@interface LoginViewController : UIViewController
 
 @end
-
