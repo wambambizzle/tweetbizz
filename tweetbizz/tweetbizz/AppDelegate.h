9d8f749289b861a2d92de326226e4b13f3985c54
Jordan Anderson <mr.janderson14@gmail.com>
2015-06-12 10:27:59 -0400

    Initial Commit of brand new project

---
+++ tweetbizz/tweetbizz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tweetbizz
+//
+//  Created by Jordan Anderson on 6/12/15.
+//  Copyright (c) 2015 Jordan Anderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
